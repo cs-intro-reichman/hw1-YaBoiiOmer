841ff714212208619eefa3b80f4cc0eb51ba9c02
Omer H <omer.h1910@gmail.com>
2025-10-29 20:22:06 +0200

    First solution of bill3

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,13 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		double totalCost = Double.parseDouble(args[3]);
+		double costForEach = Math.ceil(totalCost / 3);
+		String outputMessage = String.format("Dear %s, %s, and %s: pay %.02f Shekels each.", name3, name2, name1, costForEach);
+		System.out.println(outputMessage);
+
 	}
 }
