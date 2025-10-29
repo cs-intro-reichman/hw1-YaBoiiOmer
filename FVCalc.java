63600b3db276c514176cadf1045129eb24d31e95
Omer H <omer.h1910@gmail.com>
2025-10-29 22:15:17 +0200

    Might have fixed FVCalc issue

--- FVCalc.java
+++ FVCalc.java
@@ -4,10 +4,10 @@ public class FVCalc {
 		// Replace this comment with your code
 
 		int investment = Integer.parseInt(args[0]);
-		int years = Integer.parseInt(args[1]);
-		double interestRate = Double.parseDouble(args[2]) / 100;
+		double interestRate = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
 
-		double futureValue = investment * Math.pow((1 + interestRate), years);
+		double futureValue = investment * Math.pow((1 + interestRate/100), years);
 		// Format: After 25 years, $25000 saved at 6.0% will yield $107296
 		String outputMessage = String.format("After %d years, $%d saved at %.01f%% will yield $%d", years, investment, interestRate, ((int) futureValue));
 		System.out.println(outputMessage);
