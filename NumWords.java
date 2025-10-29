eb6615af2e1f892a2818d8c5982a10cdba178667
Omer H <omer.h1910@gmail.com>
2025-10-29 20:53:37 +0200

    Solution for num words

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,12 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int input = Integer.parseInt(args[0]);
+		int hundreds = input / 100;
+		int tens = (input / 10) % 10;
+		int units = input % 10;
+		// Format: 5 hundreds, 1 tens, and 7 ones.
+		String outputMessage = String.format("%d hundreds, 1 tens, and 7 ones.", hundreds, tens, units);
+		System.out.println(outputMessage);
 	}
 }
